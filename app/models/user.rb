63bc445ed9933f3c1d881dd17bd781507deebf84
ykmztn <you_ki1119@yahoo.co.jp>
2021-02-03 14:41:46 +0900

    ユーザー登録機能　実装

---
+++ app/models/user.rb
@@ -0,0 +1,8 @@
+class User < ApplicationRecord
+  
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
+
+  enum gender: { male: 1, female: 2, others: 3, noanswer: 4 },
+       age:    { teens: 1, twenties: 2, thirties: 3, forties: 4, more: 5}
+end
