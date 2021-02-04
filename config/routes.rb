0a663e141308e3c5cc06abfcac4e3e4905c231a7
ykmztn <you_ki1119@yahoo.co.jp>
2021-02-04 22:02:03 +0900

    検索機能実装

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: "poses#index"
+  get 'poses/search'
 
   resources :poses, only: [:index, :new, :create]
   resources :users, only: :show
