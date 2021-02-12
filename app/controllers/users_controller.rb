b49eba99eebb907512e6b43bb02d1143c5070b46
ykmztn <you_ki1119@yahoo.co.jp>
2021-02-12 14:15:45 +0900

    検索機能　修正

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,4 +4,9 @@ class UsersController < ApplicationController
     @nickname = current_user.nickname
     @poses = current_user.poses.order('created_at DESC')
   end
+
+  def search
+    @results = @p.result.order('created_at DESC')
+  end
+  
 end
